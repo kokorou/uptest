16e99332c4c9d790935ee4f7d0b57cd21dca19ff
kokorou <liwo8owil@gmail.com>
2021-03-20 21:19:32 +0900

    commit test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/UPLOAD-TEST.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://weeelb.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Weeelb
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
